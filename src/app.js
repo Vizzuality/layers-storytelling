0c9b294195fff2802572074fefe6230a3780ea3d
Alvaro Leal <pursuance@gmail.com>
2020-10-27 18:05:54 +0100

    Add Layer manager

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './app.css';
-import mapboxgl from 'mapbox-gl';
 import scrollama from 'scrollama';
 import { transformRequest } from './utils';
+import { LayerManager, Layer } from 'layer-manager/dist/components';
+import { PluginMapboxGl } from 'layer-manager';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
 const layerTypes = {
-    'fill': ['fill-opacity'],
-    'line': ['line-opacity'],
-    'circle': ['circle-opacity', 'circle-stroke-opacity'],
-    'symbol': ['icon-opacity', 'text-opacity'],
-    'raster': ['raster-opacity'],
-    'fill-extrusion': ['fill-extrusion-opacity']
+  'fill': ['fill-opacity'],
+  'line': ['line-opacity'],
+  'circle': ['circle-opacity', 'circle-stroke-opacity'],
+  'symbol': ['icon-opacity', 'text-opacity'],
+  'raster': ['raster-opacity'],
+  'fill-extrusion': ['fill-extrusion-opacity']
 }
 
 const alignments = {
-    'left': 'lefty',
-    'center': 'centered',
-    'right': 'righty'
+  'left': 'lefty',
+  'center': 'centered',
+  'right': 'righty'
 }
 
 function Chapter({ id, theme, title, image, description, currentChapterID }) {
@@ -35,27 +37,31 @@ function Chapter({ id, theme, title, image, description, currentChapterID }) {
 const App = (props) => {
   const { chapters, accessToken, style, theme, showMarkers, title, subtitle, byline, alignment, footer } = props;
   const [currentChapter, setCurrentChapter] = useState(chapters[0]);
+  const [loaded, setLoaded] = useState(false);
+  const [map, setMap] = useState(null);
+  const mapRef = useRef(null);
   const mapContainerRef = useRef(null);
+  const initialLocation = chapters[0].location;
+  const [initialLatitude, initialLongitude] = initialLocation.center;
+  const [markerPosition, setMarkerPosition] = useState({ latitude: initialLatitude, longitude: initialLongitude });
+  const initialViewport = {
+    latitude: initialLatitude,
+    longitude: initialLongitude,
+    pitch: initialLocation.pitch,
+    bearing: initialLocation.bearing,
+    zoom: initialLocation.zoom
+  };
+  const [viewport, setViewport] = useState(initialViewport);
+
+  // Set map when loaded
   useEffect(() => {
-    const mapStart = chapters[0].location;
-
-    mapboxgl.accessToken = accessToken;
-    const mapContainer = mapContainerRef && mapContainerRef.current;
-    const map = new mapboxgl.Map({
-      container: mapContainer,
-      style,
-      center: mapStart.center,
-      zoom: mapStart.zoom,
-      pitch: mapStart.pitch,
-      bearing: mapStart.bearing,
-      transformRequest: transformRequest
-    });
-
-    const marker = new mapboxgl.Marker();
-    if (showMarkers) {
-      marker.setLngLat(mapStart.center).addTo(map);
+    if (loaded && mapRef.current) {
+      setMap(mapRef.current.getMap());
     }
+    return undefined;
+  }, [mapRef, loaded, setMap]);
 
+  useEffect(() => {
     function getLayerPaintType(layer) {
       var layerType = map.getLayer(layer).type;
       return layerTypes[layerType];
@@ -63,50 +69,53 @@ const App = (props) => {
 
     function setLayerOpacity(layer) {
       var paintProps = getLayerPaintType(layer.layer);
-      paintProps.forEach(function(prop) {
-          map.setPaintProperty(layer.layer, prop, layer.opacity);
+      paintProps.forEach(function (prop) {
+        map.setPaintProperty(layer.layer, prop, layer.opacity);
       });
-    }
+    };
 
-    // instantiate the scrollama
-    const scroller = scrollama();
-
-    map.on('load', function () {
+    if (loaded && map) {
+      const scroller = scrollama();
       scroller
-      .setup({
-        step: '.step',
-        offset: 0.5,
-        progress: true
-      })
-      .onStepEnter(response => {
-        const chapter = chapters.find(c => c.id === response.element.id);
-        setCurrentChapter(chapter);
-        map.flyTo(chapter.location);
-        if (showMarkers) {
-            marker.setLngLat(chapter.location.center);
-        }
-        if (chapter.onChapterEnter.length > 0) {
+        .setup({
+          step: '.step',
+          offset: 0.5,
+          progress: true
+        })
+        .onStepEnter((response) => {
+          const chapter = chapters.find((c) => c.id === response.element.id);
+          setCurrentChapter(chapter);
+          map.flyTo(chapter.location);
+          if (showMarkers) {
+            const [markerLatitude, markerLongitude] = chapter.location.center;
+            setMarkerPosition({ latitude: markerLatitude , longitude: markerLongitude });
+          }
+          if (chapter.onChapterEnter.length > 0) {
             // const externalLayers = chapter.onChapterEnter.filter(layer => layer.type);
             // TODO: Add externalLayers to map
-            chapter.onChapterEnter.forEach(setLayerOpacity);
-        }
-      })
-      .onStepExit(response => {
-        var chapter = chapters.find(chap => chap.id === response.element.id);
-        if (chapter.onChapterExit.length > 0) {
+            chapter.onChapterEnter.forEach((layer) =>
+              setLayerOpacity(layer)
+            );
+          }
+        })
+        .onStepExit((response) => {
+          var chapter = chapters.find((chap) => chap.id === response.element.id);
+          if (chapter.onChapterExit.length > 0) {
             chapter.onChapterExit.forEach(setLayerOpacity);
-        }
-      });
-    });
-
-    window.addEventListener('resize', scroller.resize);
-  }, []);
+          }
+        });
+      window.addEventListener('resize', scroller.resize);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loaded, map]);
 
   const { id: currentChapterID } = currentChapter;
+  const onLoad = () => {
+    setLoaded(true);
+  }
 
   return (
     <div>
-      <div ref={mapContainerRef} className="absolute top right left bottom" />
       <div id="story">
         {title && (
           <div id="header" className={theme}>
@@ -130,6 +139,37 @@ const App = (props) => {
             <p>{footer}</p>
           </div>
         )}
+      </div>
+      <div ref={mapContainerRef} className="mapboxgl-map">
+        <ReactMapGL
+          ref={mapRef}
+          width="100%"
+          height="100%"
+          mapboxApiAccessToken={accessToken}
+          mapStyle={style}
+          transformRequest={transformRequest}
+          onLoad={onLoad}
+          onViewportChange={(nextViewport) => setViewport(nextViewport)}
+          scrollZoom={false}
+          dragPan={false}
+          dragRotate={false}
+          doubleClickZoom={false}
+          {...viewport}
+        >
+          {showMarkers && (
+            <Marker
+              longitude={markerPosition.longitude}
+              latitude={markerPosition.latitude}
+            />
+          )}
+          {loaded && mapRef.current && (
+            <LayerManager map={mapRef.current.getMap()} plugin={PluginMapboxGl}>
+              {/* {activeLayers.map((l) => (
+                <Layer key={l.id} {...l} />
+              ))} */}
+            </LayerManager>
+          )}
+        </ReactMapGL>
       </div>
     </div>
   );
